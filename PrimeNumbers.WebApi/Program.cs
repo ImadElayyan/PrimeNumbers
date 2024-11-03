dab498c82f9fddaec0112c393526a3829287b770
Imad Elayyan <imelayya@microsoft.com>
2024-11-03 10:59:37 +0300

    Updating bicep files

---
+++ PrimeNumbers.WebApi/Program.cs
@@ -0,0 +1,75 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+// add another endpoint to generate random prime numbers from 1 to 10000 and return only one random prime number
+app.MapGet("/randomprime", () =>
+{
+    var random = new Random();
+    var primeNumbers = Enumerable.Range(1, 10000).Where(x => IsPrime(x)).ToList();
+    var randomPrime = primeNumbers[random.Next(primeNumbers.Count)];
+    return randomPrime;
+}).WithName("GetRandomPrime")
+.WithOpenApi();
+
+// create a method to check if the number is prime or not
+
+
+ static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
